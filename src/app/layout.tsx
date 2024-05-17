4ac5494e8b4fe693817bf1fd40874468dda6bb46
somtodev <leroynnalue65@gmail.com>
2024-05-17 15:38:00 +0100

    Update: Theme | Favicon | Site Manifest

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter, Poppins } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
-const poppins = Poppins({ weight: '300', subsets: ['latin']})
+const poppins = Poppins({ weight: '300', subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: "Somtochukwu N Leroy",
@@ -17,6 +17,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png" />
+        <link rel="manifest" href="favicon/site.webmanifest" />
+      </head>
       <body className={poppins.className}>
         <main className="w-5/6 mx-auto my-5">
           {children}
